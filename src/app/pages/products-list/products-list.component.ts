d795bbbfb3cc1ebe5e907f7de280c24eb0289c02
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-03-11 19:22:59 +0545

    Fixed out of stock bug

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -9,41 +9,41 @@ import { ProductsCardComponent } from '../products-card/products-card.component'
   styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent {
-  async ngOnInit(){
-    const res=await fetch('https://fakestoreapi.com/products')
-    const data=await res.json();
-    this.products.set(data)
-  }
+  // async ngOnInit(){
+  //   const res=await fetch('https://fakestoreapi.com/products')
+  //   const data=await res.json();
+  //   this.products.set(data)
+  // }
   products=signal<Product[]>([
-    // {
-    //   id: 1,
-    //   title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 L',
-    //   price: 1299,
-    //   image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-    //   stock: 10,
-    // },
-    // {
-    //   id: 2,
-    //   title: 'Mens Casual Premium Slim Fit T-Shirts ',
-    //   price: 500,
-    //   image:
-    //     'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-    //   stock: 0,
-    // },
-    // {
-    //   id: 3,
-    //   title: 'Mens Cotton Jacket',
-    //   price: 2555,
+    {
+      id: 1,
+      title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 L',
+      price: 1299,
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+      stock: 10,
+    },
+    {
+      id: 2,
+      title: 'Mens Casual Premium Slim Fit T-Shirts ',
+      price: 500,
+      image:
+        'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+      stock: 0,
+    },
+    {
+      id: 3,
+      title: 'Mens Cotton Jacket',
+      price: 2555,
 
-    //   image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
-    //   stock: 5,
-    // },
-    // {
-    //   id: 4,
-    //   title: 'Mens Casual Slim Fit',
-    //   price: 1500,
-    //   image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
-    //   stock: 7,
-    // },
+      image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+      stock: 5,
+    },
+    {
+      id: 4,
+      title: 'Mens Casual Slim Fit',
+      price: 1500,
+      image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+      stock: 7,
+    },
   ])
 }
